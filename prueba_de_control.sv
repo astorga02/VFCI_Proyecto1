1ef0d362eddbaf52c8d54d38bb1926f73ac7b384
Mac_p_CH <mcalfred32@gmail.com>
2021-09-02 22:09:11 -0600

    implementacion de la fifo en software

--- prueba_de_control.sv
+++ prueba_de_control.sv
@@ -3,7 +3,7 @@
 `define BITS 2
 `define DEPTH 2
 `define DRIVERS 2
-`define PCKG 16
+`define PCKG 2
 `define BROD 16
 //`include "bs_gnrtr_n_rbtr"
 
@@ -20,6 +20,8 @@ module simbus;
     wire pndng [`BITS-1:0][`DRIVERS-1:0];
     wire push [`BITS-1:0][`DRIVERS-1:0];
     wire pop [`BITS-1:0][`DRIVERS-1:0];
+    wire [`PCKG-1:0] D_pop [`BITS-1:0][`DRIVERS-1:0];
+    wire [`PCKG-1:0] D_push [`BITS-1:0][`DRIVERS-1:0];
 
 
     bs_gnrtr_n_rbtr #(`BITS, `DRIVERS, `PCKG, `BROD) uut (
@@ -27,7 +29,9 @@ module simbus;
         .reset(reset),
         .pndng(pndng),
         .push(push),
-        .pop(pop)
+        .pop(pop),
+        .D_pop(D_pop),
+        .D_push(D_push)
     );
 
     initial begin
@@ -44,13 +48,35 @@ module simbus;
         prueba();
     end
 
+/////// Prograna que jecuta las pruebas y llama a la FIFO ///////
+
     task prueba();
+
+        int dato_out2, full2, empty2, rd2, wn2, c2;
+        for (int c = 0; c < 10 ; c++) begin
+            c2 = c + 23;
+            fifo(c2, rd2, wn2, dato_out2, full2, empty2);
+            $display ("Entrada de datos");
+            $monitor ("Dato numero: %g, Dato: %g", c, c2);
+        end
         
-        
-        if ($time > 10)begin
+        if ($time > 20)begin
         $display("Prueba: Tiempo límite de prueba en el test_bench alcanzado");
         $finish;
         end
     endtask
+
+/////// Declaración de la FIFO en software del sistema ////////
+
+    task fifo(input int dato_in, rd, wn, output dato_out, full, empty);
+        queue[$] = {};
+        if (wn == 1 && rd == 0)begin
+            queue.push_front(dato_in);
+        end
+        if (wn = 0 && rd == 1)begin
+            dato_out = queue.pop_front(dato_in);
+        end
+    endtask
+
 
 endmodule
