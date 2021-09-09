ad977deaa5c21c9c6df5901c413c13e064979444
Mac_p_CH <mcalfred32@gmail.com>
2021-09-09 14:36:44 -0600

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,12 +1,11 @@
 `timescale 1ns/1ps
-`include "fifo.sv"
-`include "interface_transactions.sv"
+`include "Interface_de_transacciones.sv"
+`include "agente.sv"
 `include "driver.sv"
 `include "checker.sv"
-`include "score_board.sv"
-`include "agent.sv"
+`include "monitor.sv"
+`include "generador.sv"
 `include "ambiente.sv"
-`include "test.sv"
 
 ///////////////////////////////////
 // Módulo para correr la prueba  //
@@ -29,6 +28,25 @@ module tb;
   int ancho_banda;
   int retraso_promedio;
   int resultado[0:dispositivos];
-  int buffer[0:dispositivos];
+
+
+
+always #10 clk=~clk;
+  Int_fifo #(.tama_de_paquete(tama_de_paquete),.controladores(dispositivos),.BITS(BITS)) interfaz_fifo(clk);
+  bs_gnrtr_n_rbtr uut(.clk(clk),.reset(interfaz_fifo.reset),.pndng(interfaz_fifo.pndng),.push(interfaz_fifo.push),.pop(interfaz_fifo.pop),.D_pop(interfaz_fifo.D_pop),.D_push(interfaz_fifo.D_push));
+  Ambiente #(.tama_de_paquete(tama_de_paquete),.controladores(dispositivos),.BITS(BITS),.message(message),.tam_fifo(tam_fifo),.broadcast(broadcast),.caso(caso),.opcion(opcion)) ambiente_instancia;
+  
+  initial begin
+    $dumpvars(0,bs_gnrtr_n_rbtr);
+    $dumpfile("dump.vcd");
+  end
+  
+  initial begin
+    ambiente_instancia = new();
+    ambiente_instancia.interfaz_fifo = interfaz_fifo;
+    ambiente_instancia.run(); 
+    $finish;
+  end
+ 
 
 endmodule
