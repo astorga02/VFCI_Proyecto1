c321803c2abef323905923a7b33b03fef59b2295
Mac_p_CH <mcalfred32@gmail.com>
2021-09-09 17:55:27 -0600

    Update checker.sv

--- checker.sv
+++ checker.sv
@@ -2,5 +2,17 @@
 // Checker/scoreboard: este objeto es responsable de verificar que el comportamiento del DUT sea el esperado //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 class checker #(parameter tama_de_paquete,message,broadcast,controladores,caso,opcion);
-  
+    mailbox monitor_al_checker; 
+    mailbox agente_al_checker;
+  	trans_entrada_DUT #(.tama_de_paquete(tama_de_paquete),.controladores(controladores),.caso(caso),.opcion(opcion)) item_score;
+  	trans_salida_DUT  #(.tama_de_paquete(tama_de_paquete))item_moni;
+  	bit [tama_de_paquete-1:tama_de_paquete-8] suma_mensajes [message];
+  	bit [tama_de_paquete-9:0]cajon1[$];
+    int tiempo_envio;
+    int suma_timepo;
+  	string mensaje;
+  	string pa_la_hoja;
+  	real tiempo_simulacion;
+  	int counter[0:controladores];
+
 endclass 
