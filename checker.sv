0a80d792226e36c0fc44d55d783dcd03f8688e83
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-10 00:09:35 -0600

    cambios

--- checker.sv
+++ checker.sv
@@ -7,18 +7,12 @@ class Checker#(parameter tama_de_paquete,message,broadcast,controladores,caso,op
   	TB_trans  #(.tama_de_paquete(tama_de_paquete))item_moni;
   	Bus_trans #(.tama_de_paquete(tama_de_paquete),.controladores(controladores),.caso(caso),.opcion(opcion)) item_score; 
   	bit [tama_de_paquete-1:tama_de_paquete-8] suma_mensajes [message];
-  	bit [tama_de_paquete-9:0]cajon1[$];
   	event agen_listo;
   	string mensaje,dspstv,rts,arrvng,sndng;
-  	string pa_la_hoja;
   	real tiempo_simulacion;
-  	int cajon2[$];
-  	int t;
     int tiempo_envio;
     int sumat;
-    int buffer;
-  	int counter[0:controladores];
-  	int colaDispo [0:controladores-1][0:message-1];
+
   
 
   
