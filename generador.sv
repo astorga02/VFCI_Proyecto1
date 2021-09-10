fb79421140f189221527c2c8859976aa719989e0
Mac_p_CH <mcalfred32@gmail.com>
2021-09-10 00:17:22 -0600

    agregados los casos de esquina específicos

--- generador.sv
+++ generador.sv
@@ -26,6 +26,42 @@ task run();
         llenado_especifico: //genera una transaccion con algunos datos especificos
           begin
             $display("t = %0t Generador: Se ha escogido la transaccion de llenado especifico", $time);
+            for (int i = 0; i < message; i++) begin
+              trans_entrada_DUT #(.tama_de_paquete(tama_de_paquete),.controladores(controladores),.caso(caso),.opcion(opcion)) valor; //creamos una nueva transacción
+              valor = new;
+              valor.randomize();
+              case (caso_de_esquina)//en el case se asignan algunos valores que no sean aleatorios, dependiendo del caso elegido
+                ceros:
+                  begin
+                    $display("t = %0t Ambiente: Se ha escogido el caso de opcion con un contenido de ceros", $time);
+            		valor.contenido = {8{1'b0}};
+                  end
+
+        	    unos:
+          		  begin
+            		$display("t = %0t Ambiente: Se ha escogido el caso de opcion con un contenido de unos", $time);
+            		valor.contenido = 8'b11111111;
+          		  end
+
+        		ceroyunos:
+          		  begin
+            		$display("t = %0t Ambiente: Se ha escogido el caso de opcion con un contenido de ceros y unos", $time);
+            		valor.contenido= 8'b01010101;
+          		  end
+
+        		direccion_incorrecta:
+          		 begin
+            		$display("t = %0t Ambiente: Se ha escogido el caso de opcion con una direccion incorrecta", $time);
+          		 end
+        
+        		broadcasttt:
+          		  begin
+            		$display("t = %0t Ambiente: Se ha escogido mandar mensajes con broadcast", $time);
+            		valor.destino=broadcast;
+          		  end
+        
+      		endcase
+         
           end
     endcase
   endtask
