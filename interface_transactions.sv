2bfd9b943428541572308bbfcdf4eb148597c1dd
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-11 00:46:38 -0600

    Actualización de módulo

--- interface_transactions.sv
+++ interface_transactions.sv
@@ -8,17 +8,22 @@ typedef enum{llenado_aleatorio,llenado_especifico} tipos_de_transaccion;
 
 
 
-
-
-
 interface Int_fifo#(parameter tama_de_paquete, controladores, BITS)(input bit clk); //conectar DUT con TB
   bit pndng[BITS-1:0][controladores-1:0];
   bit reset;
   bit pop[BITS-1:0][controladores-1:0];
   bit push[BITS-1:0][controladores-1:0];
-  bit [tama_de_paquete-1:0] D_pop[BITS-1:0][controladores-1:0];
-  bit [tama_de_paquete-1:0] D_push[BITS-1:0][controladores-1:0];
 endinterface
+
+
+
+class Bus_trans#(parameter tama_de_paquete,controladores,caso,opcion);
+  randc bit [tama_de_paquete-9:0] contenido;
+  rand bit [tama_de_paquete-1:tama_de_paquete-8] destino;
+  rand bit [2:0] delay;
+
+
+endclass
 
 
 
