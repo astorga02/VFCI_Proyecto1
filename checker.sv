dada15c31cfe10eea4b80a472f7f682435f049a9
Mac_p_CH <mcalfred32@gmail.com>
2021-09-11 11:23:16 -0600

    Update checker.sv

--- checker.sv
+++ checker.sv
@@ -6,7 +6,7 @@ class checker #(parameter tama_de_paquete,message,broadcast,controladores,caso,o
   mailbox monitor_al_checker; 
   mailbox agente_al_checker;
   trans_entrada_DUT #(.tama_de_paquete(tama_de_paquete),.controladores(controladores),.caso(caso),.opcion(opcion)) del_agente;
-  trans_salida_DUT  #(.tama_de_paquete(tama_de_paquete))item_moni;
+  trans_salida_DUT  #(.tama_de_paquete(tama_de_paquete)) del_monitor;
   bit [tama_de_paquete-1:tama_de_paquete-8] suma_mensajes [message];
   bit [tama_de_paquete-9:0]cajon1[$];
   int tiempo_envio;
@@ -16,6 +16,7 @@ class checker #(parameter tama_de_paquete,message,broadcast,controladores,caso,o
   real tiempo_simulacion;
   int counter[0:controladores];
   int contador;
+  int control_de_tiempos;
 
   task sumador_de_mensajes();
     for (int i=0;i<message;i++)begin
@@ -37,7 +38,12 @@ class checker #(parameter tama_de_paquete,message,broadcast,controladores,caso,o
       forever begin
         contador = contador + 1;
         monitor_al_checker.get(del_monitor);
+        del_monitor.print("Checker: Mensaje a checkear:");
+        repositorio_de_mensajes = arrayglobal.find_index with (message == del_monitor.D_push[tama_de_paquete-9:0]);
+        ttime=del_monitor.retraso-tiempo_envio;
+        sumat=control_de_tiempos+ttime;
 
+        
       end
 
   endtask
