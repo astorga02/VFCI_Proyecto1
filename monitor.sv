0ea07fbdca4d4e36af8e01a80140074032ff5f2a
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-11 00:54:41 -0600

    Actualización del módulo

--- monitor.sv
+++ monitor.sv
@@ -6,12 +6,16 @@ class Monitor#(parameter controladores,tam_fifo,BITS,tama_de_paquete);
       .controladores(controladores),
       .BITS(BITS)) interfaz_fifo;
 
-  TB_trans #(.tama_de_paquete(tama_de_paquete)) item[controladores-1:0];
+
   Fifo #(.tama_de_paquete(tama_de_paquete)) f2[controladores-1:0];
   mailbox monitor_al_checker;
 
-  
+  task run();
+    for (int p=0;p<controladores;p++)begin
+      automatic int w=p;
+
+      end
+  endtask
 
 
-  
 endclass
