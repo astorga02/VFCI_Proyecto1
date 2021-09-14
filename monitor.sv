8536829faa9017b3d19332c68d54a043b8f96e2e
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-13 19:11:57 -0600

    Actualización

--- monitor.sv
+++ monitor.sv
@@ -10,12 +10,19 @@ class Monitor#(parameter controladores,tam_fifo,BITS,tama_de_paquete);
   Fifo #(.tama_de_paquete(tama_de_paquete)) f2[controladores-1:0];
   mailbox monitor_al_checker;
 
+  
   task run();
     for (int p=0;p<controladores;p++)begin
       automatic int w=p;
-
+      fork begin
+        item[w]=new;
+        f2[w]=new(tam_fifo);
+        begin
+          @(posedge interfaz_fifo.pop[0][w])
+            #1f2[w].push(interfaz_fifo.D_push[0][w]);
+            #1f2[w].pop(item[w].D_push);
+        end
+      end
       end
   endtask
-
-
 endclass
