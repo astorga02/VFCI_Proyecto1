b62183dca015817c4a6541cdf3d822ffe9cc6d7a
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-14 12:44:36 -0600

    actualización

--- testbench.sv
+++ testbench.sv
@@ -11,7 +11,14 @@ module tb;
   parameter tama_de_paquete = 16; 
   parameter tam_fifo = 12;
   parameter caso = llenado_aleatorio;
+  parameter BITS = 1;
+  parameter message = 2;
+  parameter broadcast = {8{1'b1}};
   reg clk;
+
+
+  always #10 clk=~clk;
+  Int_fifo #(.tama_de_paquete(tama_de_paquete),.controladores(dispositivos),.BITS(BITS)) interfaz_fifo(clk);
 
 
   initial begin
