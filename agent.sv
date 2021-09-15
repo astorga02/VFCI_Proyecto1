094407578b59fb9cc7cd097f350dd839ae699921
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-15 11:04:20 -0600

    actualización

--- agent.sv
+++ agent.sv
@@ -2,22 +2,22 @@
 
 
 class Agente#(parameter tama_de_paquete,controladores,caso,opcion);
-  event agen_listo;
   mailbox agente_al_driver;
   mailbox agente_al_checker;
   mailbox generador_al_agente;
   Bus_trans #(.tama_de_paquete(tama_de_paquete),.controladores(controladores),.caso(caso),.opcion(opcion)) mensaje;
   
-
   task run();
     mensaje=new;
-    begin
+    forever begin
       generador_al_agente.get(mensaje);
       $display("t = %0tAgente: Transacción ha llegado al agente",$time);
-
+      mensaje.D_pop={mensaje.destino,mensaje.contenido};
+      $display ("t = %0tAgente: Contenido de la transacción: Numero de Fifo=%0d,Mensaje %0d",$time,mensaje.numero_fifo,mensaje.D_pop);
       agente_al_driver.put(mensaje);
       $display("t = %0tAgente: Transacción enviada al Driver",$time);
-
+      agente_al_checker.put(mensaje);
+      $display("t = %0tAgente: Transacción enviada al Checker",$time);
       end
   endtask
 
