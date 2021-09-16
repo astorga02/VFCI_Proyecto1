ee27aa81a3be5fe82447acfa9968ae33e9770d06
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-16 10:28:27 -0600

    gran actualización

--- interface_transactions.sv
+++ interface_transactions.sv
@@ -1,40 +1,50 @@
 
-//**************Tipos de prueba con casos de opcion***********************
-typedef enum {ceros, unos, ceroyunos, direccion_incorrecta,broadcasttt,cero_lejos, uno_cerca} cas_esq;
-
-
 //**************DEFINICION DE TIPOS DE TRANSACCION**************************
 typedef enum{llenado_aleatorio,llenado_especifico} tipos_de_transaccion;
 
 
+//**************Tipos de prueba con casos de opcion***********************
+typedef enum {ceros, unos, direccion_incorrecta,broadcasttt} cas_esq;
 
-interface Int_fifo#(parameter tama_de_paquete, controladores, BITS)(input bit clk); //conectar DUT con TB
+
+typedef enum {total_mess} tot_mensajes;
+
+
+
+interface Int_fifo#(parameter tama_de_paquete, controladores, BITS)(input bit clk);
   bit pndng[BITS-1:0][controladores-1:0];
   bit reset;
   bit pop[BITS-1:0][controladores-1:0];
   bit push[BITS-1:0][controladores-1:0];
+  bit [tama_de_paquete-1:0] D_pop[BITS-1:0][controladores-1:0];
+  bit [tama_de_paquete-1:0] D_push[BITS-1:0][controladores-1:0];
 endinterface
 
 
 
-class Bus_trans#(parameter tama_de_paquete,controladores,caso,opcion);
-  randc bit [tama_de_paquete-9:0] contenido;
-  rand bit [tama_de_paquete-1:tama_de_paquete-8] destino;
+class trans_entrada_DUT#(parameter tama_de_paquete,controladores,caso,opcion);
+  randc bit [tama_de_paquete-1:0] contenido;
   rand bit [2:0] delay;
   rand int numero_fifo;
+  constraint item1 {0 <= numero_fifo;  numero_fifo <= controladores-1;}
+  int destino = controladores;
+  bit [tama_de_paquete-1:0] D_push;
 
-  constraint item {0<=numero_fifo;numero_fifo<=controladores-1;destino!=numero_fifo;}
-
+  function print  (string mensaje_creado);
+    $display("t = %g %s Dato a ingresar = %d, numero_fifo = %0d, Dato ingresado = %0d", $time, mensaje_creado, this.contenido, this.numero_fifo, this.D_push);
+  endfunction
 
 endclass
 
 
-class TB_trans#(parameter tama_de_paquete);
+
+
+class trans_salida_DUT#(parameter tama_de_paquete);
   int numero_fifo;
   int retraso;
 
+
+  
 endclass
-
-
 
 
