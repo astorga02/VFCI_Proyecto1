dc39f7d375a018f6e700f64506c76e795ab4625a
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-16 18:55:12 -0600

    actualización

--- testbench.sv
+++ testbench.sv
@@ -7,8 +7,6 @@
 `include "ambiente.sv"
 
 
-
-
 module tb;
 
 
@@ -25,28 +23,38 @@ module tb;
   reg clk;
   real ancho_banda;
   real retraso_promedio;
+  real resultado[0:dispositivos];
+  real buffer[0:dispositivos];
   
   
   always #100 clk=~clk;
   Int_fifo #(.tama_de_paquete(tama_de_paquete),.controladores(dispositivos),.BITS(BITS)) interfaz_fifo(clk);
+  bs_gnrtr_n_rbtr uut(.clk(clk),.reset(interfaz_fifo.reset),.pndng(interfaz_fifo.pndng),.push(interfaz_fifo.push),.pop(interfaz_fifo.pop),.D_pop(interfaz_fifo.D_pop),.D_push(interfaz_fifo.D_push));
   Ambiente #(.tama_de_paquete(tama_de_paquete),.controladores(dispositivos),.BITS(BITS),.message(message),.tam_fifo(tam_fifo),.broadcast(broadcast),.caso(caso),.opcion(opcion)) ambiente_instancia;
-
   
-  initial begin
 
+  initial begin
     {clk,interfaz_fifo.reset}<=0;
     ambiente_instancia = new();
     ambiente_instancia.interfaz_fifo = interfaz_fifo;
     ambiente_instancia.run();
 
-
     
-    #150000
+    #150000 
     ancho_banda=(message*tama_de_paquete*1000)/ambiente_instancia.checker_instancia.tiempo_simulacion;
     retraso_promedio=ambiente_instancia.checker_instancia.suma_tiempos/ambiente_instancia.checker_instancia.contador;
     $display("[Ancho de banda] El ancho de banda total promedio fue de %0.1f x10^9 Hz,utilizando %0d dispositivos y una profundidad de Fifos de %0d",ancho_banda,dispositivos,tam_fifo);
     $display("Retraso: El retraso promedio de los mensajes que fueron recibidos fue de %0.1f ns, utilizando %0d dispositivos y una profundidad de Fifo de %0d",retraso_promedio,dispositivos,tam_fifo);
-
+    for (int i=0;i<dispositivos;i++)begin
+      for (int j=0;j<message;j++)begin
+        buffer[i]=resultado[i];
+        resultado[i]=buffer[i]+ambiente_instancia.checker_instancia.retrasos_dispositivo[i][j];
+        end
+      retraso_promedio=resultado[i]/ambiente_instancia.checker_instancia.retraso_por_dispositivo[i];
+      $display("Retraso: El retraso promedio de los %0d mensajes dirigidos al dispositivo %0d fue de %0.1f ns, utilizando %0d dispositivos y una profundidad de Fifo de %0d",ambiente_instancia.checker_instancia.retraso_por_dispositivo[i],i,retraso_promedio,dispositivos,tam_fifo);
+    end    
+    
+    $finish;
   end
  
   initial begin
@@ -55,3 +63,4 @@ module tb;
   end
 
 endmodule
+
