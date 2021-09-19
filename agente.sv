88ae9abb51f1c84bd16073f15ccb0ae6456da553
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-09-18 18:51:39 -0600

    actualización con comentarios

--- agente.sv
+++ agente.sv
@@ -6,11 +6,11 @@
 // Inico del modulo para definir el bloque del agente del ambiente //
 
 class Agente#(parameter profundidad,controladores);
-  event agen_listo;
-  mailbox generador_al_agente;
-  mailbox agente_al_driver;
-  mailbox agente_al_checker;
-  trans_entrada_DUT #(.profundidad(profundidad),.controladores(controladores)) mensaje;
+  event agen_listo; //creo un evento
+  mailbox generador_al_agente; //declaro el mailbox entre generador y agente
+  mailbox agente_al_driver; //declaro el mailbox entre agente y driver
+  mailbox agente_al_checker; //declaro el mailbox entre agente y checker
+  trans_entrada_DUT #(.profundidad(profundidad),.controladores(controladores)) mensaje; //declaro la transacción que va hacia el driver
   
   task run(); //task donde correrá el agente
     mensaje=new; //se inicializa mensaje
